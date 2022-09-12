968263ec948c489299918754dc9dd23e58c06210
Thabo <thabo.s.mngoma@gmail.com>
2022-09-12 12:58:42 -0700

    This programme prints out a string into the std output stream

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+Description: main - The main function prints a string to the std output stream
+Return: returns 0 when printed succesfully
+*/
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return (0);
+}
