0688fcda453f2fe5cc7c4169a6ad12e7daa8b2b2
Thabo <thabo.s.mngoma@gmail.com>
2022-09-12 11:30:52 -0700

    Further Betty syntax improvements

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *The main funtion prints to standard output a string 
- *The main funtion returns 0 when it has succesfully printed out  text
- */
+*The main funtion prints to standard output a string 
+*The main funtion returns 0 when it has succesfully printed out  text
+*/
 int main(void)
 {
-  char myStr[] = "\"Programming is like building a multilingual puzzle";
-  puts(myStr);
-  return 0; 
+char myStr[] = "\"Programming is like building a multilingual puzzle";
+puts(myStr);
+return (0); 
 }
