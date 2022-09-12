bcf343c9750bc7fd6cd12f1fd17ccc4802b875ea
Thabo <thabo.s.mngoma@gmail.com>
2022-09-12 14:59:56 -0700

    removed whitespace trails

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,16 @@
 */
 int main(void)
 {
-char chars;  
-int integers;  
-long longs;  
+char chars;
+int integers;
+long longs;
 long long doubleLong;
 float floats;
-  
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(chars));  
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integers));  
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(chars));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integers));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longs));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(doubleLong));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floats));
-return (0); 
+return (0);
 }
