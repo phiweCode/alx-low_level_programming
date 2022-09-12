9636ea844cb14b713fc5174a15053e7a4df63b72
Thabo <thabo.s.mngoma@gmail.com>
2022-09-12 11:22:37 -0700

    Improved the code by formatting it Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 
+/**
+ *The main funtion prints to standard output a string 
+ *The main funtion returns 0 when it has succesfully printed out  text
+ */
+
 int main()
 {
- 
   char myStr[]="\"Programming is like building a multilingual puzzle";
-
   puts(myStr);
-  
   return 0; 
 }
