c6266388067446cb299c61fa88e20f27501f4cc9
Thabo <thabo.s.mngoma@gmail.com>
2022-09-14 04:31:38 -0700

    This programme prints out english characters to the standard output

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Description: print_alphabet prints out English alphabets
+ * to the standard output
+ */
+
+void print_alphabet(void)
+{
+int chr;
+
+for (chr='a' ; chr<='z' ; ++chr)
+{
+_putchar(chr);
+}
+}
