e5ece2556f6c68fba6d976f1235d9ea1aecc2ed5
Thabo <thabo.s.mngoma@gmail.com>
2022-09-14 20:38:24 -0700

    This program checks if a char is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * Descsription: _isalpha - checks if the char is an alphabet
+ *@c: is letter char to be checked
+ *Return: returns 0 when succesful and 1 when unsuccesful
+ */
+
+int _isalpha(int c)
+{
+int alphBool = isalpha(c);
+
+if( alphBool > 0)
+{
+return (1);
+}else
+{
+return (0);
+}
+}
