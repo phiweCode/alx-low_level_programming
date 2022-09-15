c806381871a4468d088115a683d775c77ad39b4b
Thabo <thabo.s.mngoma@gmail.com>
2022-09-14 20:01:12 -0700

    optimized for Betty coding style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- *Description: print_alphabet_x10 - prints to the stdout 
+ *Description: print_alphabet_x10 - prints to the stdout
  *the first 10 English alphabet
  */
 void print_alphabet_x10(void)
 {
-  int iter, chr;
+int iter, chr;
 
 for (iter = 0 ; iter < 10 ; iter++)
 {
 for (chr = 'a' ; chr<='z' ; ++chr)
 _putchar(chr);
- _putchar('\n');
+_putchar('\n');
 }
 
 }
