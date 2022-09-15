6291cd2795d3e9c4313cbcc9e724cfd14e125f1c
Thabo <thabo.s.mngoma@gmail.com>
2022-09-14 20:25:00 -0700

    fixed Betty coding style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  *Description: _islower - checks if a char is lowercase
- *@c - is the input variable to undergo checking
+ *@c: is the input variable to undergo checking
  *Return: 1 is retuned when char is lowercase or 0 otherwise
  */
 
 int _islower(int c)
 {
-if( c >= 97 && c <= 122)
+if (c >= 97 && c <= 122)
 {
 return (1);
 }
