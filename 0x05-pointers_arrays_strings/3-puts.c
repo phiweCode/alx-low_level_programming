2f308d909b72cd12d30c62b0c65e4df82d596c2a
Thabo <thabo.s.mngoma@gmail.com>
2022-09-19 17:06:57 -0700

    This programme prints out data to the stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.c"
+
+/**
+ *Description: _puts - prints out a string to the stdout
+ *@str: input character pointer paramenter
+ */
+
+void _puts(char *str)
+{
+_putchar(str);
+_putchar('\n');
+}
