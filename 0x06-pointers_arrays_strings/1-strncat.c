3c9de6998a142c93814ccd7782a1c2db4cacdd85
Thabo <thabo.s.mngoma@gmail.com>
2022-09-21 14:46:44 -0700

    added description

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,10 +2,11 @@
 #include <string.h>
 
 /**
- * Description: strncat - concatinates two strings of specified length
+ * Description: _strncat - concatinates two strings of specified length
  * @dest: is the char input string pointer to be appended.
  * @src: is the char input string pointer to append the other
  * Return: returns the concatinated string
+ * @n: the length of the characters of the string to be appended
  */
 
 char *_strncat(char *dest, char *src, int n)
