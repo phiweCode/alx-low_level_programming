2118f3864b3d22e4ce52a206f2fc6767479e6518
Thabo <thabo.s.mngoma@gmail.com>
2022-09-21 14:30:31 -0700

    This programe returns the concatinated string pointer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#including "main.h"
+#including <string.h>
+
+/**
+ * Description: strncat - concatinates two strings of specified length
+ * @dest: is the char input string pointer to be appended.
+ * @src: is the char input string pointer to append the other
+ * Return: returns the concatinated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+return (strncat(dest,src,n))
+}
