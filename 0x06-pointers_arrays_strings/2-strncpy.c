5319fd664d859a4f075317a6e5f847b4e5e7c6f7
Thabo <thabo.s.mngoma@gmail.com>
2022-09-22 01:20:06 -0700

    This programming copies a string a returns it

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * Description: _strncpy - copies a string 'src' along with null termination
+ * to 'dest' poiter
+ * @dest: pointer to be copied to
+ * @src: pointer carrying the string to be pointed to 
+ * @n: is maximum number of chars to be pointed to
+ * Return: returns the pointer containg the copied string 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+return ((dest, src, n));
+}
