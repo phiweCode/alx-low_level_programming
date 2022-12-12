b1ccc1c5a69fd2e47217a6a6c91dfb31fef72153
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-12 14:31:04 +0200

    adding dirs

---
+++ 0x02-functions_nested_loops/if.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a,b ;
+
+  a = 10;
+  b = 21;
+
+  if(a > b)
+    {
+      printf("a is smaller than b\n");
+	}else
+    {
+      printf("b is smaller than a\n");
+    }
+  return(0);
+  
+ 
+}
