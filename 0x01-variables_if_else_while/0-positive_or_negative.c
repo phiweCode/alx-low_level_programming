f1d7afd7e962629da041151f263f4f2884b555ac
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-12 20:34:07 +0200

    removed whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 /*
 *Description: This program checks whether a number
-*is positive or negative and prints out the 
+*is positive or negative and prints out the
 *the results thereafter returns 0
 */
 int main(void)
