79ec95f1fa0a37f67d39b74efb46932720c803ec
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-12 14:48:26 +0200

    fixed empty spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,15 @@ int main(void)
   srand(time(0));
   n = rand() - RAND_MAX / 2;
   /* your code goes there */
-  if(n==0)
-    {
-      printf("0 is zero\n");
-    }else if(n>0)
-    {
-      printf("%d is positive\n",n);
-    }else
-    {
-      printf("%d is negative\n",n);
-    }
-
-  
-  return (0);
+if(n==0)
+{
+  printf("0 is zero\n");
+}else if(n>0)
+{
+  printf("%d is positive\n",n);
+}else
+{
+  printf("%d is negative\n",n);
+}
+return (0);
 }
