b9aacd61701256ab3f9d82c89eabedde062c2706
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-13 01:35:40 +0200

    fixed the comparing var

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ if ( lastDigit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n",n,lastDigit);
 }
-else if ( n == 0)
+else if ( lastDigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n",n,lastDigit);
 }
