f56ed333625f75c50f713c8cfb041eb1b906e2af
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-13 01:11:36 +0200

    removed the last  condition

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ else if ( n == 0)
 {
 printf("Last digit of %d is %d and is 0\n",n,lastDigit);
 }
-else if ( n < 6 && n != 0)
+else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n",n,lastDigit);
 }
