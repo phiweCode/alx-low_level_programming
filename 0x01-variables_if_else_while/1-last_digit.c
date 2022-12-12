53032116bccc2e975d92ce54797b90c0caa1705c
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-13 00:53:27 +0200

    This program compares the last digit of an interger and prints out the results conditionaly based on the resulting value

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+* main - This program compares the last digit of
+* an integer and prints out results
+* Return: is always 0
+*/
+
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+
+int last_digit;
+
+last_digit = n % 10;
+
+if ( last_digit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n",n,last_digit);
+}
+else if ( n == 0)
+{
+printf("Last digit of %d is %d and is 0\n",n,last_digit);
+}
+else if ( n < 6 && n != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n",n,last_digit);
+}
+
+ return (0);
+}
