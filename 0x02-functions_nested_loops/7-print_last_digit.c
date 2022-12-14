679f8d0b2f46907cadaa09af89168b0306a3afc6
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-14 03:28:19 +0200

    This program returns the last digit of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+* main - This program returns the
+* last digit of an integer 
+* @n: is an integer arg 
+* Return: returns the value of the 
+* last digit of the integer var
+*/
+
+int print_last_digit(int n)
+{
+
+int lastDigit;
+
+lastDigit = n%10;
+
+return (lastDigit);
+
+}
