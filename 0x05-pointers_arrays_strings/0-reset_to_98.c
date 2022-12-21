6c6efb64947422e8c72a12b3027a17532d0bd96c
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-21 21:01:48 +0200

    fixed bug

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * Description: reset_to_98 - resets an input value
@@ -7,5 +8,5 @@
 
 void reset_to_98(int *n)
 {
-*n = 98;
+  *n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 int _putchar(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void print_rev(char *s)
+void print_rev(char *s);
 
 
   
