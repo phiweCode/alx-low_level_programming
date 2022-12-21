d879536f73f5a72869ba056294ef225c8696d2d4
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-21 20:39:17 +0200

    newly edited files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * Description: reset_to_98 - resets an input value
- *@n: this is an interger pointer parameter
- */
+* Description: reset_to_98 - resets an input value
+* @n: this is an interger pointer parameter
+*/
 
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *Description: swap_int - swaps the values of the input args
- *@a: is the interger pointer parameter
- *@b: is the interger pointer parameter
- */
+*Description: swap_int - swaps the values of the input args
+*@a: is the interger pointer parameter
+*@b: is the interger pointer parameter
+*/
 
 void swap_int(int *a, int *b)
 {
+
 int temp1;
 int temp2;
 
