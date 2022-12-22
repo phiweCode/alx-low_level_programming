c49d0a41134c0f9aaef900000b98f09c7a77bd09
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-22 23:28:20 +0200

    This program compares two strings and then return an interger depending on the comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings 
+ * @s1: is the first string compared
+ * @s2: is the second string compared
+ * return: returns 0 if the strings are 
+ * equal, a positive or negatice one if
+ * they are not
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+  int result = strcmp(s1,s2);
+  return (result); 
+ 
+}
