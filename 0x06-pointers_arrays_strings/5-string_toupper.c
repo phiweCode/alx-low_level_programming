b8d700e16be9570fd0d547c99bab2ddcc5e06eab
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-24 01:03:44 +0200

    another fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -15,13 +15,10 @@ char *string_toupper(char *s)
 int ind;
 
 for (ind = 0; ind != '\0'; ind++)
-{
-
-printf("This is char s before %c", s[ind]); 
+{ 
   
 s[ind] = toupper(s[ind]);
 
-printf("This is char s after %c", s[ind]);
 }
  
 return (s);
