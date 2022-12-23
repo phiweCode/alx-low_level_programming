3d75bbbdbd96a284597f28eb2d8c8d8cad157186
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-23 04:42:25 +0200

    adjusted for betty style

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,36 +3,33 @@
 #include <string.h>
 
 /**
- * _strcmp - compares two strings 
- * @s1: is the first string compared
- * @s2: is the second string compared
- * return: returns 0 if the strings are 
- * equal, a positive or negatice one if
- * they are not
- */
+* _strcmp - compares two strings
+* @s1: is the first string compared
+* @s2: is the second string compared
+* return: returns 0 if the strings are
+* equal, a positive or negatice one if
+* they are not
+*/
 
 int _strcmp(char *s1, char *s2)
 {
-  int i;
-  int index;
+int i;
+int index;
 
-  for (i = 0; i != '\0'; i++)
-  {
-  if (s1[i] - s2[i] == 0)
-  {
+for (i = 0; i != '\0'; i++)
+{
+if (s1[i] - s2[i] == 0)
+{
 
-  index = i; 
+index = i;
 
-  }
-  else
-  {
-  break; 
-  }
-  
-  }
-
-  return (s1[index] - s2[index]); 
+}
+else
+{
+break;
 }
 
+}
 
-
+return (s1[index] - s2[index]);
+}
