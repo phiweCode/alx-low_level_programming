68e29901973352332e24756a59fb407b0aafadba
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-25 13:28:30 +0200

    This progamm prints base 10 numbers from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints base numbers
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar(i);
+}
+
+return (0);
+
+} 
