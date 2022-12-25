ceb984e360c4def3db397db3758689ecc0443da5
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-25 18:58:34 +0200

    this program prints a list of comma separated integers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a comma 
+ * separared list of integers
+ * Return: returns 0 upon success
+ */
+
+int main(void)
+{
+
+int numbers;
+
+for (numbers = 0; numbers < 10; numbers++)
+{
+
+putchar((char)numbers + '0');
+
+if (numbers != 9)
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+putchar('\n');
+return (0); 
+ 
+}
