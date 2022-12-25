cedec8113091a1ebb930319851a849ba13220215
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-25 18:13:23 +0200

    removed empty spaces

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - prints base 10 digit 
- * Return: returns 0 upon 
+ * main - prints base 10 digit
+ * Return: returns 0 upon
  */
 
 int main(void)
 {
 
 int digit;
-  
+
 for (digit = 0; digit < 10; digit++)
 {
 putchar((char)digit + '0');
 }
 
 putchar('\n');
-return (0); 
+return (0);
 
 }
