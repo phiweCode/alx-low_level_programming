11027259a9f254ea73430822a963acf1baf59780
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-26 10:57:56 +0200

    optimized for betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ for (hourSD = 0; hourSD < 3; hourSD++)
 {
 for (hourFD = 0; hourFD < 10; hourFD++)
 {
-if (hourSD <= 2 && hourFD <=3)
+if (hourSD <= 2 && hourFD <= 3)
 {
 for (min = 0; min < 6; min++)
 {
-for(sec = 0; sec < 10; sec++)
+for (sec = 0; sec < 10; sec++)
 {
 putchar((char)hourSD + '0');
 putchar((char)hourFD + '0');
