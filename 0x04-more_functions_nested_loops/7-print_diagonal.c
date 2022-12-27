53fd395cb7bb66164ca1dafc884fa9550721aad8
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-27 22:52:36 +0200

    this program prints a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints 
+ * diagonal line 
+ * @n: length of diagonal 
+ */
+
+void print_diagonal(int n)
+{
+
+int i;
+int temp;
+  
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+temp = 0;
+while(temp < i)
+{
+_putchar(' ');
+temp++;
+}
+_putchar('\\');
+_putchar('\n');
+}
+}  
+}
+
