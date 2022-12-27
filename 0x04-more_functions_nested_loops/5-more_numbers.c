bc88f5c0ceaa6c8337aac772325598521a3d3135
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-27 21:44:50 +0200

    fixed the while loop condition and the betty cond

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,17 +14,17 @@ int count;
 
 count = 0;
 
-while (count <= 10)
+while (count < 10)
 {
 for (i = 0; i <= 14; i++)
 {
-if (i/10 >= 1)
+if (i / 10 >= 1)
 {
 _putchar(i / 10 + '0');
 }
 _putchar(i % 10 + '0');
 }
-_putchar('\n');
+_putchar('\n'); 
 count ++;
 }
 }
