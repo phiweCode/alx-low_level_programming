05617c4677e5317442bd84b8c794980510e920b8
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-28 12:07:45 +0200

    fixed betty

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,9 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
- * main - this is a fizzbuzz 
- */
+* main - this is a fizzbuzz
+* Return: returns 0 on completion
+*/
 
 int main(void)
 {
@@ -11,13 +12,13 @@ int i;
 int iter;
 char fizz[50] = "Fizz";
 char buzz[50] = "Buzz";
-char fizzBuzz[50] = "FizzBuzz"; 
+char fizzBuzz[50] = "FizzBuzz";
 
 for (i = 1; i <= 100; i++)
 {
-if(i % 3 == 0 && i % 5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
-for(iter = 0; fizzBuzz[iter] != '\0'; iter++)
+for (iter = 0; fizzBuzz[iter] != '\0'; iter++)
 {
 putchar(fizzBuzz[iter]);
 }
@@ -25,23 +26,23 @@ putchar(' ');
 }
 else if (i % 3 == 0)
 {
-for(iter = 0; fizz[iter] != '\0'; iter++)
+for (iter = 0; fizz[iter] != '\0'; iter++)
 {
 putchar(fizz[iter]);
 }
-putchar(' '); 
+putchar(' ');
 }
-else if(i % 5 == 0)
+else if (i % 5 == 0)
 {
-for(iter = 0; buzz[iter] != '\0'; iter++)
+for (iter = 0; buzz[iter] != '\0'; iter++)
 {
 putchar(buzz[iter]);
 }
-putchar(' ');	    
+putchar(' ');
 }
 else
 {
-if(i / 10 > 0)
+if (i / 10 > 0)
 {
 putchar(i / 10 + '0');
 }
