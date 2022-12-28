5f7f11c6f39e8e120e855d1cd6e5fd9fbf031b5a
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-28 11:55:44 +0200

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * main - this is a fizzbuzz 
+ * Return: returns 0 upon completion
+ */
+
+int main(void)
+{
+
+int i;
+int iter;
+char fizz[50] = "Fizz";
+char buzz[50] = "Buzz";
+char fizzBuzz[50] = "FizzBuzz"; 
+
+for (i = 1; i <= 100; i++)
+{
+if(i % 3 == 0 && i % 5 == 0)
+{
+for(iter = 0; fizzBuzz[iter] != '\0'; iter++)
+{
+_putchar(fizzBuzz[iter]);
+}
+_putchar(' ');
+}
+else if (i % 3 == 0)
+{
+for(iter = 0; fizz[iter] != '\0'; iter++)
+{
+_putchar(fizz[iter]);
+}
+_putchar(' '); 
+}
+else if(i % 5 == 0)
+{
+for(iter = 0; buzz[iter] != '\0'; iter++)
+{
+_putchar(buzz[iter]);
+}
+_putchar(' ');	    
+}
+else
+{
+if(i / 10 > 0)
+{
+_putchar(i / 10 + '0');
+}
+_putchar(i % 10 + '0');
+_putchar(' ');
+}
+} 
+return (0);
+}
