22622848afc0022c06f16eb1dc2128da640e0dd0
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-29 11:19:34 +0200

    this program prints the last half of a string's characters

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - prints last half
+ * of string characters
+ * @str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+
+int i;
+int strlen = _strlen(str);
+
+if (strlen % 2 == 0)
+{
+
+for (i = _strlen(str) - 1; i >= strlen/2; i--)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
+else
+{
+
+for (i = _strlen(str) - 1; i >= (strlen - 1) / 2; i--)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _putchar(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,5 +9,6 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-  
+void puts_half(char *str);
+
 #endif 
