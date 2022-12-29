ef0c6d95fc9277857a6e53497f0a1d8b1ab0652a
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-29 14:22:56 +0200

    optimized for betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,8 +4,8 @@
 /**
  * print_array - prints n elements
  * of an array
- * @a: array 
- * @n: number of elements to be 
+ * @a: array
+ * @n: number of elements to be
  * prnted
  */
 
@@ -13,10 +13,10 @@ void print_array(int *a, int n)
 {
 
 int i;
-  
+
 for (i = 0; i < n; i++)
 {
-printf("%d",a[i]);
+printf("%d ",a[i]);
 
 if (i + 1 != n)
 {
@@ -25,4 +25,3 @@ putchar(',');
 }
 putchar('\n');
 }
-
