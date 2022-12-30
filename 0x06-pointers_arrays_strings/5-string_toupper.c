afb9b9800622d041b70c9612bc67f0bc5139966b
phiweCode <thabo.s.mngoma@gmail.com>
2022-12-30 14:34:31 +0200

    refactored

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -15,14 +15,14 @@ char *string_toupper(char *s)
 int ind;
 int len;
 
-len = (int)strlen(s); 
- 
+len = (int)strlen(s);
+
 for (ind = 0; ind < len; ind++)
-{ 
+{
 
 s[ind] = toupper(s[ind]);
- 
+
 }
- 
+
 return (s);
 }
