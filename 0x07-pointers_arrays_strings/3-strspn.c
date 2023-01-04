cefcb30cb6c86c0f99ff1534aa9d2522332584f9
phiweCode <thabo.s.mngoma@gmail.com>
2023-01-05 00:21:46 +0200

    This program returns the length of the longest prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strspn - prints the longest 
+ * prefix in a string 
+ * @s: is the source string 
+ * @accept: is the set of character 
+ * to be matched 
+ * Return: returns the length of 
+ * the longest prefix  
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+unsigned int i;
+unsigned int j;
+unsigned int count;
+
+count = 0;
+
+for (i = 0; i < strlen(s); i++)
+{
+          
+if (count == i)
+{
+          
+for (j = 0; j < strlen(accept); j++)
+{
+    
+if (s[i] == accept[j])
+{
+count ++;
+break; 
+}
+}
+}
+else if (count < i)
+{
+if (count >= strlen(accept) )
+{
+return (count);
+}
+else 
+{
+return (0);
+break; 
+}    
+}
+}
+return (0);
+}
