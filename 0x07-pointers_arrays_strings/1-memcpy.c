f2e4feb1a7864d024a317ad8514586bd80e3b4e5
phiweCode <thabo.s.mngoma@gmail.com>
2023-01-04 10:53:46 +0200

    adjusted for betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * _memcpuy - copies memory of 
- * variables 
- * @dest: param to which memory 
- * will be copied to 
- * @src: source of memory to be 
- * copied 
- * @n: bytes of memory to be 
- * copied 
- * Return: returns a pointer 
+ * _memcpy - copies memory of
+ * variables
+ * @dest: param to which memory
+ * will be copied to
+ * @src: source of memory to be
+ * copied
+ * @n: bytes of memory to be
+ * copied
+ * Return: returns a pointer
  * to dest
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n) 
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
 unsigned int i;
 
 for (i = 0; i < n; i++)
 {
-dest[i] = src[i]; 
+dest[i] = src[i];
 }
 return (dest);
 }
