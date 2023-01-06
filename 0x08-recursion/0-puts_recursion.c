22dde4bd82f30afcbeaf71e3f7bd8dfb497e7e61
phiweCode <thabo.s.mngoma@gmail.com>
2023-01-06 21:40:42 +0200

    removed whitespaces

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
-#include "main.h" 
+#include "main.h"
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 
 /**
- * _puts_recursion - prints out a string 
- *  @s: is the input string paramgit 
+ * _puts_recursion - prints out a string
+ *  @s: is the input string paramgit
 */
 
-void _puts_recursion(char *s) 
-{ 
- 
+void _puts_recursion(char *s)
+{
+
 if (*s == '\0')
-{ 
-return; 
-} 
-putchar(*s); 
-_puts_recursion(s + 1); 
-   
-} 
+{
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+_putchar('\n');
+}
